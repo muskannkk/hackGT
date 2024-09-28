4eb0cb96c5fb3bee59fb9d6050058cd7a1c55c26
muskannkk <muskannkk@gmail.com>
2024-09-28 00:31:04 -0400

    profile updates

--- skinCare/models.py
+++ skinCare/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # Create your models here.
+class Day(models.Model):
+    #dailyPicture = models.ImageField(upload_to='day/')
+    notes = models.TextField()
+    dayProducts = models.TextField()
+    nightProducts = models.TextField()
+    diet = models.TextField()
+
+class Profile(models.Model):
+   user = models.OneToOneField(User, on_delete=models.CASCADE)
+   #profile_picture = models.ImageField(upload_to='profile_pictures', blank=True)
+   models.ManyToManyField(Day, blank=True)
+
+   def __str__(self):
+       return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+   if created:
+       Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+   instance.profile.save()
