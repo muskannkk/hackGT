f27350bbddad5f90ebb2532f38462f4086912277
muskannkk <muskannkk@gmail.com>
2024-09-29 02:07:27 -0400

    saving notes working!!

--- skinCare/models.py
+++ skinCare/models.py
@@ -13,10 +13,36 @@ class Day(models.Model):
     nightProducts = models.TextField()
     diet = models.TextField()
 
+class MonthOverview(models.Model):
+    janNotes = models.TextField()
+    febNotes = models.TextField()
+    marNotes = models.TextField()
+    aprNotes = models.TextField()
+    mayNotes = models.TextField()
+    junNotes = models.TextField()
+    julNotes = models.TextField()
+    augNotes = models.TextField()
+    sepNotes = models.TextField()
+    octNotes = models.TextField()
+    novNotes = models.TextField()
+    decNotes = models.TextField()
+
 class Profile(models.Model):
    user = models.OneToOneField(User, on_delete=models.CASCADE)
    #profile_picture = models.ImageField(upload_to='profile_pictures', blank=True)
    days = models.ManyToManyField(Day, blank=True)
+   januaryNotes = models.TextField(default="")
+   februaryNotes = models.TextField(default="")
+   marchNotes = models.TextField(default="")
+   aprilNotes = models.TextField(default="")
+   mayNotes = models.TextField(default="")
+   juneNotes = models.TextField(default="")
+   julyNotes = models.TextField(default="")
+   augustNotes = models.TextField(default="")
+   septemberNotes = models.TextField(default="")
+   octoberNotes = models.TextField(default="")
+   novemberNotes = models.TextField(default="")
+   decemberNotes = models.TextField(default="")
 
    def __str__(self):
        return self.user.username
